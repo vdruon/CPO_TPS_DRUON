1720ab3df7fb51ad2acb3a2e0f734087a9c5aed3
Victor DRUON <victor.druon@epfedu.fr>
2023-09-25 13:00:07 +0200

    Création d'un nouveau projet versionné

---
+++ TP0_DRUON/Exo 1/src/exo1/Exo1.java
@@ -0,0 +1,24 @@
+/* 
+* DRUON 
+* FG2 TDA 
+* TP0 
+* 18/09/2023
+*/
+
+package exo1;
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    System.out.println("Bonjour");
+   
+    }
+    
+}
+
+
+
+
