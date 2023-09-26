14e40556c04c91bcf98da0f0448f37941dbc24be
Victor DRUON <victor.druon@epfedu.fr>
2023-09-26 21:17:11 +0200

    Création d'un nouveau projet versionné

---
+++ TP0_DRUON/TP1_guessMyNumber_DRUON/src/tp1_guessmynumber_druon/TP1_guessMyNumber_DRUON.java
@@ -0,0 +1,196 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_guessmynumber_druon;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * DRUON Victor TDA 26/09/2023
+ *
+ * @author vdruo
+ */
+public class TP1_guessMyNumber_DRUON {
+
+    /**
+     * @param args the command line arguments
+     *
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        // instruction permettant de générer des nombres aléatoires
+        Random generateurAleat = new Random();
+
+        // creer le compteur, afin de savoir en combien de coup le joueur a trouvé le bon nombre (le compteur commence a 1 car le joueur vient de saisir son premier nombre
+        int nb_tour = 1;
+
+        
+        // créer les différents modes facile, normal, difficile
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Quel mode voulez vous choisir ? \n\n\nLe mode FACILE : \nIntervalle : entre 0 et 100 \nNombre de coups : illimite \nMessages d'aide ameliores \n\nLe mode NORMAL : \nIntervalle : entre 0 et 100 \nNombre de coups : 10 \nMessages d'aide \n\nLe mode DIFFICILE : \nIntervalle : entre 0 et 300 \nNombre de coups : 10 \nMessages d'aide \n\n\nTapez 1 pour FACILE\nTapez 2 pour NORMAL\nTapez 3 pour DIFFICILE");
+        int mode = sc.nextInt();
+
+        switch (mode) {
+
+            
+            
+            
+            
+            // l'utilisateur choisit FACILE
+            case 1:
+
+                // générons maintenant un entier alétoire inférieur ou égale à 100 (positif)
+                int n = generateurAleat.nextInt(100);
+
+                // l'utilisateur doit trouver ce nombre, demandons lui quel nombre veut-il choisir
+                System.out.println("Choisir un nombre entier entre 0 et 100");
+                int n_user = sc.nextInt();
+
+                // créons une boucle while "tant que les nombres sont différents" pour que l'utilisateur trouve le bon nombre
+                while (n != n_user) {
+
+                    // si le nombre de l'utilisateur est plus grand, afficher "Vraiment trop grand !" ou "Trop grand !" 
+                    if (n < n_user) {
+                        if (n_user - n >30) {
+                            System.out.println("Vraiment trop grand !");
+                        }
+                        
+                        else {
+                        System.out.println("Trop grand !");
+                        }
+                    } // si le nombre de l'utilisateur est plus petit, afficher "Vraiment trop petit !" ou "Trop petit !"
+                    else {
+                        if (n - n_user > 30) {
+                            System.out.println("Vraiment trop petit");
+                        }
+                        else {
+                            System.out.println("Trop petit !");
+                        }
+                    }
+
+                    // demander à l'utilisateur de choisir un autre nombre
+                    System.out.println("Choisir un nombre entier entre 0 et 100");
+                    n_user = sc.nextInt();
+
+                    // mettre à jour le compteur
+                    nb_tour += 1;
+
+                }
+
+                //L'utilisateur a trouvé le bon nombre, félicitons-le
+                System.out.println("Bravo vous avez trouve le bon nombre " + n + " en " + nb_tour + " coup !");
+
+                
+                
+                
+                
+                
+                
+                
+            // l'utilisateur choisit NORMAL
+            case 2:
+
+                // générons maintenant un entier alétoire inférieur ou égale à 100 (positif)
+                n = generateurAleat.nextInt(100);
+
+                // l'utilisateur doit trouver ce nombre, demandons lui quel nombre veut-il choisir
+                System.out.println("Choisir un nombre entier entre 0 et 100");
+                n_user = sc.nextInt();
+
+                // créons une boucle while "tant que les nombres sont différents" pour que l'utilisateur trouve le bon nombre
+                while (n != n_user) {
+
+                    // si le nombre de l'utilisateur est plus grand, afficher "Trop grand !" 
+                    if (n < n_user) {
+                        System.out.println("Trop grand !");
+                        }
+                    
+                    // si le nombre de l'utilisateur est plus petit, afficher "Vraiment trop petit !" ou "Trop petit !"
+                    else {
+                            System.out.println("Trop petit !");
+                        }
+                   
+                    // demander à l'utilisateur de choisir un autre nombre
+                    System.out.println("Choisir un nombre entier entre 0 et 100");
+                    n_user = sc.nextInt();
+
+                    // mettre à jour le compteur
+                    nb_tour += 1;
+                    
+                    // arreter la boucle while si le joueur dépasse les 10 coups
+                    if (nb_tour >= 11 ) {
+                        break;
+                    }
+
+                }
+
+                // arreter la partie si le joueur dépasse les 10 coups
+                    if (nb_tour >= 11 ) {
+                        System.out.println("Vous avez depasse le nombre de coups maximal. Retentez votre chance pour faire mieux ! ");
+                        break;
+                    }
+                    
+                //L'utilisateur a trouvé le bon nombre, félicitons-le
+                System.out.println("Bravo vous avez trouve le bon nombre " + n + " en " + nb_tour + " coup !");
+                
+      
+                
+                
+                
+                
+            // l'utilateur chosit DIFFICILE
+            case 3:
+                
+                // générons maintenant un entier alétoire inférieur ou égale à 300 (positif)
+                n = generateurAleat.nextInt(300);
+
+                // l'utilisateur doit trouver ce nombre, demandons lui quel nombre veut-il choisir
+                System.out.println("Choisir un nombre entier entre 0 et 300");
+                n_user = sc.nextInt();
+
+                // créons une boucle while "tant que les nombres sont différents" pour que l'utilisateur trouve le bon nombre
+                while (n != n_user) {
+
+                    // si le nombre de l'utilisateur est plus grand, afficher "Trop grand !" 
+                    if (n < n_user) {
+                        System.out.println("Trop grand !");
+                        }
+                    
+                    // si le nombre de l'utilisateur est plus petit, afficher "Vraiment trop petit !" ou "Trop petit !"
+                    else {
+                            System.out.println("Trop petit !");
+                        }
+                   
+                    // demander à l'utilisateur de choisir un autre nombre
+                    System.out.println("Choisir un nombre entier entre 0 et 100");
+                    n_user = sc.nextInt();
+
+                    // mettre à jour le compteur
+                    nb_tour += 1;
+                    
+                    // arreter la boucle while si le joueur dépasse les 10 coups
+                    if (nb_tour >= 11 ) {
+                        break;
+                    }
+
+                }
+
+                // arreter la partie si le joueur dépasse les 10 coups
+                    if (nb_tour >= 11 ) {
+                        System.out.println("Vous avez depasse le nombre de coups maximal. Retentez votre chance pour faire mieux ! ");
+                        break;
+                    }
+                    
+                //L'utilisateur a trouvé le bon nombre, félicitons-le
+                System.out.println("Bravo vous avez trouve le bon nombre " + n + " en " + nb_tour + " coup !");
+                
+                
+
+        }
+
+    }
+
+}
