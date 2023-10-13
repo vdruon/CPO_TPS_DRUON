407b5bf3d33f2b00112deef88fd6aa8b0193bfbb
Victor DRUON <victor.druon@epfedu.fr>
2023-10-13 10:43:03 +0200

    Création d'un nouveau projet versionné

---
+++ TP0_DRUON/TP2_relation_1_DRUON/src/tp2_relation_1_druon/TP2_relation_1_DRUON.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_relation_1_druon;
+
+/**
+ *
+ * @author vdruo
+ */
+public class TP2_relation_1_DRUON {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    Voiture uneClio = new Voiture ("Clio", "Renault", 5 ) ;
+    Voiture uneAutreClio = new Voiture ("Clio", "Renault", 5 ) ;
+    Voiture une2008 = new Voiture ("2008", "Peugeot", 6 ) ;
+    Voiture uneMicra = new Voiture ("Micra", "Nissan", 4 ) ;
+    Personne bob = new Personne("Bobby", "Sixkiller");
+    Personne reno = new Personne("Reno", "Raines");
+    System.out.println("liste des voitures disponibles "+ uneClio + "\n" + uneAutreClio + "\n" + une2008 + "\n" + uneMicra ) ;
+        
+     
+    
+    bob.liste_voitures[0] = uneClio ;
+    bob.nbVoitures = 1 ;
+    uneClio.Proprietaire = bob ;    
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
