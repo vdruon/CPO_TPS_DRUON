d4bc2620b4165f0aab7933d64d7c868bbb21bb43
Victor DRUON <victor.druon@epfedu.fr>
2023-10-13 10:16:57 +0200

    Création d'un nouveau projet versionné

---
+++ TP0_DRUON/TP2_manip_DRUON/src/tp2_manip_druon/Moussaka.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_druon;
+
+/**
+ *
+ * @author vdruo
+ */
+public class Moussaka {
+    
+    int nbCalories;
+    
+    public Moussaka(int nb_calories) {
+        nbCalories = nb_calories ;
+    }
+    
+    
+    
+}
---
+++ TP0_DRUON/TP2_manip_DRUON/src/tp2_manip_druon/Tartiflette.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_druon;
+
+/**
+ *
+ * @author vdruo
+ */
+public class Tartiflette {
+    
+    int nbCalories;
+    
+    public Tartiflette(int nb_calories) {
+        nbCalories = nb_calories ;
+    }
+    
+    
+}
